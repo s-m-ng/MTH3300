4e3db1aa83fa7a82e2db61a3baf5001a296f166d
s-m-ng <sn4green@gmail.com>
2024-05-15 13:32:51 -0400

    Update trapezoid.py
    Added name and contributors

--- Homework/trapezoid.py
+++ Homework/trapezoid.py
@@ -1,11 +1,11 @@
 ##******************************************************************************
 # trapezoid.py
 #******************************************************************************
-# Name: 
+# Name: Sin Ng
 #******************************************************************************
 # Collaborators/outside sources used 
 #(IMPORTANT! Write "NONE" if none were used):
-#
+# NONE
 #
 #
 # Reminder: you are to write your own code.
